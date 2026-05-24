10acc2c81541a3de326dc17487dee3bbf0bb16e6
tangtongshan <1726048962@qq.com>
2026-05-24 16:23:57 +0800

    跟新数据流

---
+++ RTL_from_TTS_TEMP/TOP/accelerator_top.v
@@ -0,0 +1,18 @@
+module accelerator (
+    input   clk_h,      //高速时钟
+    input   clk_l,      //低速时钟
+    input   hrst_n,     //高速复位
+    input   lrst_n,      //低速复位
+    input   start,      //启动信号
+    output  done        //完成信号
+);
+/*---------- PARAMETER ----------*/
+parameter DATA_WIDTH = 16;
+
+/*---------- WIRE/REG  ----------*/
+wire
+/*---------- INSTANCE ----------*/
+//FIFO
+
+
+endmodule
